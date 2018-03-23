5e9e5fadaeb682451ff0f3ff433f777720a08012
pankaja92 <pankajanc@gmail.com>
2018-03-23 09:25:37 +0530

    Fix3

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
+const API_KEY = require('./config/keys').API_KEY;
+const MovieDb = require("moviedb-promise");
+const moviedb = new MovieDb(API_KEY);
 
 const PORT = process.env.PORT || 5000;
 
@@ -25,11 +28,29 @@ if (cluster.isMaster) {
   app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
   // Answer API requests.
-  app.get('/api', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
+  app.get("/api/search/:movie", async (req, res) => {
+    const movie = req.params.movie;
+    try {
+      const result = await moviedb.searchMovie({ query: movie });
+      movieList = result.results;
+      res.send(movieList);
+    } catch (err) {
+      console.error(err);
+    }
   });
 
+  app.get("/api/movie/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+      const result = await moviedb.movieInfo({ id: id });
+      movie = result;
+      res.send(movie);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
