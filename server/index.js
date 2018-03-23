4f23774266214f6b8e8257890ee27fface87327b
pankaja92 <pankajanc@gmail.com>
2018-03-23 09:30:31 +0530

    Fix4

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
-const API_KEY = require('./config/keys').API_KEY;
+const express = require("express");
+const path = require("path");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+const API_KEY = require("./config/keys").API_KEY;
 const MovieDb = require("moviedb-promise");
 const moviedb = new MovieDb(API_KEY);
 
@@ -17,46 +17,35 @@ if (cluster.isMaster) {
     cluster.fork();
   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${
+        worker.process.pid
+      } exited: code ${code}, signal ${signal}`
+    );
   });
-
 } else {
   const app = express();
 
   // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+  app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
 
   // Answer API requests.
-  app.get("/api/search/:movie", async (req, res) => {
-    const movie = req.params.movie;
-    try {
-      const result = await moviedb.searchMovie({ query: movie });
-      movieList = result.results;
-      res.send(movieList);
-    } catch (err) {
-      console.error(err);
-    }
+  app.get("/api", function(req, res) {
+    res.set("Content-Type", "application/json");
+    res.send('{"message":"Hello from the custom server!"}');
   });
 
-  app.get("/api/movie/:id", async (req, res) => {
-    const id = req.params.id;
-    try {
-      const result = await moviedb.movieInfo({ id: id });
-      movie = result;
-      res.send(movie);
-    } catch (err) {
-      console.log(err);
-    }
-  });
-
-  
   // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+  app.get("*", function(request, response) {
+    response.sendFile(
+      path.resolve(__dirname, "../react-ui/build", "index.html")
+    );
   });
 
-  app.listen(PORT, function () {
-    console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+  app.listen(PORT, function() {
+    console.error(
+      `Node cluster worker ${process.pid}: listening on port ${PORT}`
+    );
   });
 }
